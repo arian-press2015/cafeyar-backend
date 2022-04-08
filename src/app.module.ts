e5024f397d8baeacad962329f45b1320465767db
AP2015 <arian.press2015@gmail.com>
2022-04-08 14:19:11 +0430

    refactor: sort routes alphabetically

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,84 +1,84 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { UserModule } from './user/user.module';
-import { HostModule } from './host/host.module';
-import { TableModule } from './table/table.module';
-import { ImageTypeModule } from './imageType/imageType.module';
 import { CategoryModule } from './category/category.module';
-import { SubcategoryModule } from './subcategory/subcategory.module';
-import { ProductModule } from './product/product.module';
-import { InvoiceModule } from './invoice/invoice.module';
-import { FaqModule } from './faq/faq.module';
-import { IngredientModule } from './ingredient/ingredient.module';
-import { ProductStarModule } from './productStar/productStar.module';
-import { ProductImageModule } from './productImage/productImage.module';
 import { CategoryImageModule } from './categoryImage/categoryImage.module';
-import { QrImageModule } from './qrImage/qrImage.module';
-import { HostImageModule } from './hostImage/hostImage.module';
-import { FeatureModule } from './feature/feature.module';
-import { PurchaseLevelModule } from './purchaseLevel/purchaseLevel.module';
-import { PurchasePlanModule } from './purchasePlan/purchasePlan.module';
-import { PurchaseLifetimeModule } from './purchaseLifetime/purchaseLifetime.module';
-import { UserImageModule } from './userImage/userImage.module';
-import { RateModule } from './rate/rate.module';
-import { TipModule } from './tip/tip.module';
-import { PersonnelModule } from './personnel/personnel.module';
-import { OrderModule } from './order/order.module';
-import { UserFeatureModule } from './userFeature/userFeature.module';
-import { PurchaseModule } from './purchase/purchase.module';
-import { PaymentModule } from './payment/payment.module';
 import { DiscountModule } from './discount/discount.module';
-import { VersionModule } from './version/version.module';
-import { SubcategoryDiscountModule } from './subcategoryDiscount/subcategoryDiscount.module';
-import { RoleModule } from './role/role.module';
-import { PermissionModule } from './permission/permission.module';
-import { OwnerRoleModule } from './ownerRole/ownerRole.module';
-import { OwnerPermissionModule } from './ownerPermission/ownerPermission.module';
+import { FaqModule } from './faq/faq.module';
+import { FeatureModule } from './feature/feature.module';
+import { HostModule } from './host/host.module';
+import { HostImageModule } from './hostImage/hostImage.module';
+import { ImageTypeModule } from './imageType/imageType.module';
+import { IngredientModule } from './ingredient/ingredient.module';
+import { InvoiceModule } from './invoice/invoice.module';
+import { OrderModule } from './order/order.module';
 import { OwnerModule } from './owner/owner.module';
+import { OwnerPermissionModule } from './ownerPermission/ownerPermission.module';
+import { OwnerRoleModule } from './ownerRole/ownerRole.module';
+import { PaymentModule } from './payment/payment.module';
+import { PermissionModule } from './permission/permission.module';
+import { PersonnelModule } from './personnel/personnel.module';
+import { ProductModule } from './product/product.module';
+import { ProductImageModule } from './productImage/productImage.module';
+import { ProductStarModule } from './productStar/productStar.module';
+import { PurchaseModule } from './purchase/purchase.module';
 import { PurchaseDiscountModule } from './purchaseDiscount/purchaseDiscount.module';
+import { PurchaseLevelModule } from './purchaseLevel/purchaseLevel.module';
+import { PurchaseLifetimeModule } from './purchaseLifetime/purchaseLifetime.module';
+import { PurchasePlanModule } from './purchasePlan/purchasePlan.module';
+import { QrImageModule } from './qrImage/qrImage.module';
+import { RateModule } from './rate/rate.module';
+import { RoleModule } from './role/role.module';
+import { SubcategoryModule } from './subcategory/subcategory.module';
+import { SubcategoryDiscountModule } from './subcategoryDiscount/subcategoryDiscount.module';
+import { TableModule } from './table/table.module';
+import { TipModule } from './tip/tip.module';
+import { UserModule } from './user/user.module';
+import { UserFeatureModule } from './userFeature/userFeature.module';
+import { UserImageModule } from './userImage/userImage.module';
+import { VersionModule } from './version/version.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../..', 'public'),
     }),
-    UserModule,
-    HostModule,
-    TableModule,
-    ImageTypeModule,
     CategoryModule,
-    SubcategoryModule,
-    ProductModule,
-    InvoiceModule,
-    FaqModule,
-    IngredientModule,
-    ProductStarModule,
-    ProductImageModule,
     CategoryImageModule,
-    QrImageModule,
-    HostImageModule,
-    FeatureModule,
-    PurchaseLevelModule,
-    PurchasePlanModule,
-    PurchaseLifetimeModule,
-    UserImageModule,
-    RateModule,
-    TipModule,
-    PersonnelModule,
-    OrderModule,
-    UserFeatureModule,
-    PurchaseModule,
-    PaymentModule,
     DiscountModule,
-    VersionModule,
-    SubcategoryDiscountModule,
-    RoleModule,
-    PermissionModule,
-    OwnerRoleModule,
-    OwnerPermissionModule,
+    FaqModule,
+    FeatureModule,
+    HostModule,
+    HostImageModule,
+    ImageTypeModule,
+    IngredientModule,
+    InvoiceModule,
+    OrderModule,
     OwnerModule,
+    OwnerPermissionModule,
+    OwnerRoleModule,
+    PaymentModule,
+    PermissionModule,
+    PersonnelModule,
+    ProductModule,
+    ProductImageModule,
+    ProductStarModule,
+    PurchaseModule,
     PurchaseDiscountModule,
+    PurchaseLevelModule,
+    PurchaseLifetimeModule,
+    PurchasePlanModule,
+    QrImageModule,
+    RateModule,
+    RoleModule,
+    SubcategoryModule,
+    SubcategoryDiscountModule,
+    TableModule,
+    TipModule,
+    UserModule,
+    UserFeatureModule,
+    UserImageModule,
+    VersionModule,
   ],
   controllers: [],
   providers: [],
