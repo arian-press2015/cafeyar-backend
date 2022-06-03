4b7c2fb086041f05a00451343813379d5a6f6e89
AP2015 <arian.press2015@gmail.com>
2022-06-04 01:05:18 +0430

    refactor: clean package.json

--- test/jest-e2e.ts
+++ test/jest-e2e.ts
@@ -1,6 +1,5 @@
 process.env.DATABASE_URL =
   'mysql://ap2015_test:ap2015_test@localhost:3306/cafeyar_test';
-process.env.NODE_CONFIG_DIR = './dist/config';
 
 export default {
   moduleFileExtensions: ['js', 'json', 'ts'],
