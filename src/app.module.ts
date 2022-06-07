f08e9ed4ab071d85aef727b32a7660251a50a96d
AP2015 <arian.press2015@gmail.com>
2022-06-07 23:08:26 +0430

    feat: separate configs for test and development

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,6 +45,7 @@ import { VersionModule } from './version/version.module';
       rootPath: join(__dirname, '../..', 'public'),
     }),
     ConfigModule.forRoot({
+      envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
       isGlobal: true,
       load: [configuration],
       cache: true,
