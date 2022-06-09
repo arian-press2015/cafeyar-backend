d2508755e51ec8e1eb55584bb37e7f533644787d
AP2015 <arian.press2015@gmail.com>
2022-06-09 22:47:26 +0430

    refactor: remove unused files

--- src/config/test.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  env: 'dev',
-  port: 3000,
-  secret: 'dsaifasdg8hi43kl51n3rfqlkfed9090ur512j3wqklfs9zcxybiolk243dfa63',
-  redis_url: 'redis://AP2015:AP2015@localhost:6379',
-};
